4acece3f777a957bdc6ed11e6b44583f0fc9acff
Danila Kost <kostdani@fit.cvut.cz>
2022-06-14 03:58:22 +0200

    Implementation Parser.cpp

--- Parser.cpp
+++ Parser.cpp
@@ -1,5 +1,9 @@
 #include "Parser.hpp"
 
+
+
+
+
 Parser::Parser()
     : MilaContext()
     , MilaBuilder(MilaContext)
@@ -10,39 +14,15 @@ Parser::Parser()
 bool Parser::Parse()
 {
     getNextToken();
+    tree=Start();
     return true;
 }
 
 const llvm::Module& Parser::Generate()
 {
-
-    // create writeln function
-    {
-      std::vector<llvm::Type*> Ints(1, llvm::Type::getInt32Ty(MilaContext));
-      llvm::FunctionType * FT = llvm::FunctionType::get(llvm::Type::getInt32Ty(MilaContext), Ints, false);
-      llvm::Function * F = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, "writeln", MilaModule);
-      for (auto & Arg : F->args())
-          Arg.setName("x");
-    }
-
-    // create main function
-    {
-        llvm::FunctionType * FT = llvm::FunctionType::get(llvm::Type::getInt32Ty(MilaContext), false);
-        llvm::Function * MainFunction = llvm::Function::Create(FT, llvm::Function::ExternalLinkage, "main", MilaModule);
-
-        // block
-        llvm::BasicBlock * BB = llvm::BasicBlock::Create(MilaContext, "entry", MainFunction);
-        MilaBuilder.SetInsertPoint(BB);
-
-      // call writeln with value from lexel
-      MilaBuilder.CreateCall(MilaModule.getFunction("writeln"), {
-              llvm::ConstantInt::get(MilaContext, llvm::APInt(32, m_Lexer.numVal()))
-      });
-
-      // return 0
-      MilaBuilder.CreateRet(llvm::ConstantInt::get(llvm::Type::getInt32Ty(MilaContext), 0));
-    }
-
+    //tree->Write(this);
+    SymbTable st(this);
+    tree->Generate(st);
     return this->MilaModule;
 }
 
@@ -55,5 +35,6 @@ const llvm::Module& Parser::Generate()
  */
 int Parser::getNextToken()
 {
+
     return CurTok = m_Lexer.gettok();
 }
